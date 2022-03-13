9546c2cff296ce853575b963253599afd3f606cb
JosephatNgugi <Josephat.muriu@student.moringaschool.com>
2022-03-13 09:54:35 +0300

    Add a hover effect on portfolio images

--- js/script.js
+++ js/script.js
@@ -13,5 +13,70 @@ $(function(){
     $(".toggleShow").click(function(){
         $(".imgHide1, .imgHide2, .imgHide3").toggle(800);
         $(".showText1, .showText2, .showText3").toggle(1000)
-    })
+    });
+
+    $(".image1").hover(function() {
+        $(this).animate({opacity: 0.2}, 500);
+        $(".imgText1").show(500);
+    }, function() {
+        $(this).animate({opacity: 1.0}, 500);
+        $(".imgText1").hide(500);
+
+    });
+    $(".image2").hover(function() {
+        $(this).animate({opacity: 0.2}, 500);
+        $(".imgText2").show(500);
+    }, function() {
+        $(this).animate({opacity: 1.0}, 500);
+        $(".imgText2").hide(500);
+
+    });
+        $(".image3").hover(function() {
+        $(this).animate({opacity: 0.2}, 500);
+        $(".imgText3").show(500);
+    }, function() {
+        $(this).animate({opacity: 1.0}, 500);
+        $(".imgText3").hide(500);
+
+    });
+        $(".image4").hover(function() {
+        $(this).animate({opacity: 0.2}, 500);
+        $(".imgText4").show(500);
+    }, function() {
+        $(this).animate({opacity: 1.0}, 500);
+        $(".imgText4").hide(500);
+
+    });
+        $(".image5").hover(function() {
+        $(this).animate({opacity: 0.2}, 500);
+        $(".imgText5").show(500);
+    }, function() {
+        $(this).animate({opacity: 1.0}, 500);
+        $(".imgText5").hide(500);
+
+    });
+        $(".image6").hover(function() {
+        $(this).animate({opacity: 0.2}, 500);
+        $(".imgText6").show(500);
+    }, function() {
+        $(this).animate({opacity: 1.0}, 500);
+        $(".imgText6").hide(500);
+
+    });
+        $(".image7").hover(function() {
+        $(this).animate({opacity: 0.2}, 500);
+        $(".imgText7").show(500);
+    }, function() {
+        $(this).animate({opacity: 1.0}, 500);
+        $(".imgText7").hide(500);
+
+    });
+        $(".image8").hover(function() {
+        $(this).animate({opacity: 0.2}, 500);
+        $(".imgText8").show(500);
+    }, function() {
+        $(this).animate({opacity: 1.0}, 500);
+        $(".imgText8").hide(500);
+
+    });
 });
